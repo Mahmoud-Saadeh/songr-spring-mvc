6f2413fd853b07022ee630c339a6b62bd0d189fc
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-07-12 21:12:45 +0300

    12/7

--- src/main/java/com/example/songr/Album.java
+++ src/main/java/com/example/songr/Album.java
@@ -5,10 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//@Entity
+@Entity
 public class Album {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String artist;
@@ -16,7 +16,7 @@ public class Album {
     private int length;
     private String imageUrl;
 
-//    public Album(){}
+    public Album(){}
     public Album(String title, String artist, int songCount, int length, String imageUrl) {
         this.title = title;
         this.artist = artist;
--- src/main/java/com/example/songr/AlbumRoutes.java
+++ src/main/java/com/example/songr/AlbumRoutes.java
@@ -16,31 +16,31 @@ public class AlbumRoutes {
     private AlbumRepository albumRepository;
     @GetMapping("/albums")
     String getAlbums(Model model){
-        ArrayList<Album> albums = new ArrayList<>();
+//        ArrayList<Album> albums = new ArrayList<>();
 
-        albums.add(new Album("Song1","Artist1", 2,120, "https://cdn.pixabay.com/photo/2017/08/02/05/49/recording-2570056__340.jpg"));
-        albums.add(new Album("Song2","Artist2", 7,180, "https://cdn.pixabay.com/photo/2015/10/12/15/13/boy-984293__340.jpg"));
-        albums.add(new Album("Song3","Artist3", 5,240, "https://cdn.pixabay.com/photo/2015/06/07/18/41/birds-800671_960_720.jpg"));
-//        List<Album> albums = albumRepository.findAll();
+//        albums.add(new Album("Song1","Artist1", 2,120, "https://cdn.pixabay.com/photo/2017/08/02/05/49/recording-2570056__340.jpg"));
+//        albums.add(new Album("Song2","Artist2", 7,180, "https://cdn.pixabay.com/photo/2015/10/12/15/13/boy-984293__340.jpg"));
+//        albums.add(new Album("Song3","Artist3", 5,240, "https://cdn.pixabay.com/photo/2015/06/07/18/41/birds-800671_960_720.jpg"));
+        List<Album> albums = albumRepository.findAll();
 //        System.out.println("albums.size()"+albums.size());
-//        model.addAttribute("albumClass", new Album());
+        model.addAttribute("albumClass", new Album());
         model.addAttribute("albums",albums);
         model.addAttribute("albumsSize",albums.size());
 
         return "albums";
     }
-//    @PostMapping("/albums")
-//    public String postAlbum(@ModelAttribute Album album, Model model){
-//        albumRepository.save(album);
-//
-//        List<Album> albums = albumRepository.findAll();
-//        model.addAttribute("albumClass", new Album());
-//        model.addAttribute("albums",albums);
-//
-//        System.out.println("albums.size() PostMapping   "+albums.size());
-////        System.out.println("albums.size() PostMapping   "+albums.get(0));
-////        System.out.println("the greeting is >>> " + album.getContent());
-////        model.addAttribute("album", album);
-//        return "redirect:/albums";
-//    }
+    @PostMapping("/albums")
+    public String postAlbum(@ModelAttribute Album album, Model model){
+        albumRepository.save(album);
+
+        List<Album> albums = albumRepository.findAll();
+        model.addAttribute("albumClass", new Album());
+        model.addAttribute("albums",albums);
+
+        System.out.println("albums.size() PostMapping   "+albums.size());
+//        System.out.println("albums.size() PostMapping   "+albums.get(0));
+//        System.out.println("the greeting is >>> " + album.getContent());
+        model.addAttribute("album", album);
+        return "redirect:/albums";
+    }
 }
